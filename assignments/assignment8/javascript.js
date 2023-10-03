956ab58a9bbcb0ca45f9064b843641c285bb488b
jenna-stover.github.io <jenna.stover810@gmail.com>
2023-10-02 22:44:17 -0400

    added styling

--- assignments/assignment8/javascript.js
+++ assignments/assignment8/javascript.js
@@ -5,28 +5,16 @@ const moveStickfigure = () => {
 
 const displayData = () => {
     const fundsRaised = parseInt(document.getElementById("funds-raised").value);
-    const resultDiv = document.getElementById("result");
-
-    if(isBlank(fundsRaised, "error-funds") | 
-    isNotNumber(fundsRaised, "error-funds"))
-    {
-        return false; 
-    }
 
     const goal = 10000.00;
 
     const percentageRaised = (fundsRaised / goal) * 100;
     const clampedPercentage = Math.min(100, Math.max(0, percentageRaised));
 
-    resultDiv.style.background = `linear-gradient(to top, #790967 ${clampedPercentage}%, #ffffff ${clampedPercentage}%)`;
+    const progressBarFill = document.getElementById("progress-bar-fill");
+    
+    progressBarFill.style.height = `${clampedPercentage}%`;
 
-}
-
-const isNotNumber = (data, id) => {
-    if (data.trim() == "" || isNaN(data)) {
-        document.getElementById(id).classList.remove("hide");
-        return true;
-    }
 }
 
 
