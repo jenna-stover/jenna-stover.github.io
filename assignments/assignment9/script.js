e99f9a24a202de98597cc45efea7d0dc02159408
jenna-stover.github.io <jenna.stover810@gmail.com>
2023-10-09 16:16:12 -0400

    updated styling

--- assignments/assignment9/script.js
+++ assignments/assignment9/script.js
@@ -1,6 +1,13 @@
+const quotes = [];
 
+const initializeQuotesArray = () => {
+    const quoteElements = document.querySelectorAll("#slideshow .hidden");
+    quoteElements.forEach((quoteElement) => {
+      quotes.push(quoteElement.textContent);
+    });
+  };
 
-const addQuote = () => {
+const slide = () => {
     const currentQuote = document.querySelector("#slideshow :not(.hidden)");
     currentQuote.classList.add("hidden");
 
@@ -11,11 +18,37 @@ const addQuote = () => {
     }
 
     nextQuote.classList.remove("hidden");
-
-
 }
 
 
+const rainbowColors = ["red", "orange", "yellow", "green", "blue", "purple"];
+let i = 0;
+
+const createRainbow = () => {
+    const rainbowDiv = document.getElementById("results-rainbow");
+    const potOfGold = document.getElementById("pot-of-gold");
+
+    if(i < rainbowColors.length) {
+        const color = rainbowColors[i];
+        const rainbowStrip = document.createElement("div");
+        rainbowStrip.classList.add("rainbow-strip");
+        rainbowStrip.style.backgroundColor = color;
+        rainbowDiv.appendChild(rainbowStrip);
+
+        i++;
+
+        setTimeout(createRainbow, 1000);
+    } 
+    else {
+        setTimeout(() => {
+            potOfGold.style.display = "block";
+        }, 1000);
+    }
+  
+}
+
 window.onload = () => {
-    document.getElementById("button-rainbow").onclick = addQuote;
+    initializeQuotesArray();
+    setInterval(slide, 2000);
+    document.getElementById("button-rainbow").onclick = createRainbow;
 }
