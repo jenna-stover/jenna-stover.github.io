2aa681f7dae0ceed310b486c3a9583e1c27514b9
jenna-stover.github.io <jenna.stover810@gmail.com>
2023-10-31 04:40:30 -0400

    updated styling

--- projects/part5/internshipPage/script.js
+++ projects/part5/internshipPage/script.js
@@ -2,46 +2,88 @@ const toggleNav = () => {
     document.getElementById("nav-items").classList.toggle("hide-small");
 };
 
-// const internships= [];
+const internshipsArr= [];
 
-// const addInternship = () => {
-//     const companyInput = document.getElementById("company-input").value;
-//     const appLinkInput = document.getElementById("link-input").value;
-//     const locationInput = document.getElementById("location-input").value;
-//     const deadlineInput = document.getElementById("deadline-input").value;
+const addInternship = () => {
+    const companyInput = document.getElementById("company-input").value;
+    const appLinkInput = document.getElementById("link-input").value;
+    const locationInput = document.getElementById("location-input").value;
+    const deadlineInput = document.getElementById("deadline-input").value;
 
-//     const table = document.getElementById("internship-table");
-//     const newRow = table.insertRow(-1);
+    internshipsArr.push({
+        company: companyInput,
+        link: appLinkInput,
+        location: locationInput,
+        deadline: deadlineInput,
+    });
 
-//     const cell1 = newRow.insertCell(0);
-//     const cell2 = newRow.insertCell(1);
-//     const cell3 = newRow.insertCell(2);
-//     const cell4 = newRow.insertCell(3);
+    document.getElementById("company-input").value = "";
+    document.getElementById("link-input").value = "";
+    document.getElementById("location-input").value = "";
+    document.getElementById("deadline-input").value = "";
 
-//     cell1.innerHTML = companyInput;
-//     cell2.innerHTML = `<a href="${appLinkInput}">${companyInput} Internship</a>`;
-//     cell3.innerHTML = locationInput;
-//     cell4.innerHTML = deadlineInput;
+    showInternships();
+};
 
-//     internships.push({
-//         company: companyInput,
-//         link: appLinkInput,
-//         location: locationInput,
-//         deadline: deadlineInput,
-//     });
-
-//     document.getElementById("company-input").value = "";
-//     document.getElementById("link-input").value = "";
-//     document.getElementById("location-input").value = "";
-//     document.getElementById("deadline-input").value = "";
-// };
 
 const getInternships = async () => {
     const url = "https://jenna-stover.github.io/json/internships.json";
-}
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+const showInternships = async () => {
+    let internships = await getInternships();
+    let internshipsSection = document.getElementById("internships-section");
+
+    internships.forEach((internship) => {
+        internshipsSection.append(getInternshipItem(internship))
+    });
+};
+
+const getInternshipItem = (internship) => {
+    let section = document.createElement("section");
+    section.classList.add("internship");
+
+    let img = document.createElement("img");
+    img.src = internship.img;
+    section.append(img);
+
+    let div = document.createElement("div");
+    section.append(div);
+
+    let h3 = document.createElement("h3");
+    h3.innerText = internship.company;
+    div.append(h3);
+
+    let ul = document.createElement("ul");
+    div.append(ul);
+    
+    let applicationLink = document.createElement("a");
+    applicationLink.href = internship.link;
+    applicationLink.textContent = "Application Link";
+    applicationLink.target = "_blank"; 
+    
+    ul.append(applicationLink);
+    ul.append(getLi(`Internship Location: ${internship.location}`));
+    ul.append(getLi(`Deadline: ${internship.deadline}`));
+    return section;
+};
+
+const getLi = data => {
+    const li = document.createElement("li");
+    li.textContent = data;
+
+    return li;
+};
 
 
 window.onload = () => {
     document.getElementById("hamburger").onclick = toggleNav;
     document.getElementById("internship-button").onclick = addInternship;
+    showInternships();
 };
