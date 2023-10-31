d41b9b4c7a3beed6440f731fda559a49440207e9
jenna-stover.github.io <jenna.stover810@gmail.com>
2023-10-31 03:22:47 -0400

    updated styling

--- projects/part5/internshipPage/script.js
+++ projects/part5/internshipPage/script.js
@@ -2,39 +2,43 @@ const toggleNav = () => {
     document.getElementById("nav-items").classList.toggle("hide-small");
 };
 
-const internships= [];
+// const internships= [];
 
-const addInternship = () => {
-    const companyInput = document.getElementById("company-input").value;
-    const appLinkInput = document.getElementById("link-input").value;
-    const locationInput = document.getElementById("location-input").value;
-    const deadlineInput = document.getElementById("deadline-input").value;
+// const addInternship = () => {
+//     const companyInput = document.getElementById("company-input").value;
+//     const appLinkInput = document.getElementById("link-input").value;
+//     const locationInput = document.getElementById("location-input").value;
+//     const deadlineInput = document.getElementById("deadline-input").value;
 
-    const table = document.getElementById("internship-table");
-    const newRow = table.insertRow(-1);
+//     const table = document.getElementById("internship-table");
+//     const newRow = table.insertRow(-1);
 
-    const cell1 = newRow.insertCell(0);
-    const cell2 = newRow.insertCell(1);
-    const cell3 = newRow.insertCell(2);
-    const cell4 = newRow.insertCell(3);
+//     const cell1 = newRow.insertCell(0);
+//     const cell2 = newRow.insertCell(1);
+//     const cell3 = newRow.insertCell(2);
+//     const cell4 = newRow.insertCell(3);
 
-    cell1.innerHTML = companyInput;
-    cell2.innerHTML = `<a href="${appLinkInput}">${companyInput} Internship</a>`;
-    cell3.innerHTML = locationInput;
-    cell4.innerHTML = deadlineInput;
+//     cell1.innerHTML = companyInput;
+//     cell2.innerHTML = `<a href="${appLinkInput}">${companyInput} Internship</a>`;
+//     cell3.innerHTML = locationInput;
+//     cell4.innerHTML = deadlineInput;
 
-    internships.push({
-        company: companyInput,
-        link: appLinkInput,
-        location: locationInput,
-        deadline: deadlineInput,
-    });
+//     internships.push({
+//         company: companyInput,
+//         link: appLinkInput,
+//         location: locationInput,
+//         deadline: deadlineInput,
+//     });
 
-    document.getElementById("company-input").value = "";
-    document.getElementById("link-input").value = "";
-    document.getElementById("location-input").value = "";
-    document.getElementById("deadline-input").value = "";
-};
+//     document.getElementById("company-input").value = "";
+//     document.getElementById("link-input").value = "";
+//     document.getElementById("location-input").value = "";
+//     document.getElementById("deadline-input").value = "";
+// };
+
+const getInternships = async () => {
+    const url = "https://jenna-stover.github.io/json/internships.json";
+}
 
 
 window.onload = () => {
